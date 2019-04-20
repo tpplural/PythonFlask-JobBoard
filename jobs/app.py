244948b5ea147afd0f7abf72d54ce2db2b3d5b76
qxr6047 <pondo5as>
2019-04-20 21:19:16 +0200

    l4

--- jobs/app.py
+++ jobs/app.py
@@ -31,4 +31,5 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql('select job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name from job join employer on employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
